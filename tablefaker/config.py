fafc9734e4915dca183383098f38f50b2ad6be01
Necati Arslan <necatia@gmail.com>
2023-11-03 15:42:49 -0400

    config validation

--- tablefaker/config.py
+++ tablefaker/config.py
@@ -4,6 +4,10 @@ from . import util
 
 class Config:
     def __init__(self, file_path):
+        if not path.isabs(file_path):
+            file_path = path.abspath(file_path)
+
+        util.log(f"received config is {file_path}")
         self.file_path = file_path
         self.load_config_file()
         self.validate_config()
@@ -16,4 +20,34 @@ class Config:
             raise Exception(f"{self.file_path} file not found")
     
     def validate_config(self):
-        pass
+        if "tables" not in self.config:
+            raise Exception(f"Config file should have tables node")
+        
+        if len(self.config["tables"]) == 0:
+            raise Exception(f"Config file should contain at least 1 table")
+        
+        for table in self.config["tables"]:
+            if "table_name" not in table:
+                raise Exception(f"Table should have a table_name attribute")
+            
+            table_name = table["table_name"]
+
+            if "row_count" not in table:
+                raise Exception(f"{table_name} table should have a row_count attribute")
+
+            if "columns" not in table:
+                raise Exception(f"{table_name} table should have a columns attribute")
+
+            if len(table["columns"]) == 0:
+                raise Exception(f"{table_name} table should have at least 1 table")
+            
+            for column in table["columns"]:
+                if "column_name" not in column:
+                    raise Exception(f"{table_name} table have a column without a column_name attribute")
+            
+                column_name = column["column_name"]
+                
+                if "data" not in column:
+                    raise Exception(f"{table_name} table {column_name} column do not have a data attribute")
+        
+        util.log(f"config file is validated")
--- tests/test_tablefaker.py
+++ tests/test_tablefaker.py
@@ -3,7 +3,7 @@ sys.path.append("/Users/necatiarslan/GitHub/table-faker")
 
 from tablefaker import tablefaker
 
-#tablefaker.to_csv("tests/test_table.yaml", "./tests/exports")
+tablefaker.to_csv("tests/test_table.yaml", "./tests/exports")
 #tablefaker.to_json("tests/test_table.yaml", "./tests/exports")
 #tablefaker.to_excel("tests/test_table.yaml", "./tests/exports")
 #tablefaker.to_parquet("tests/test_table.yaml", "./tests/exports")
