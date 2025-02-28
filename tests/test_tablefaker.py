90af17b8596a7dfcd94f85f710f01227d7af61fc
Necati Arslan <necatia@gmail.com>
2025-02-28 09:15:54 -0500

    test file

--- tests/test_tablefaker.py
+++ tests/test_tablefaker.py
@@ -15,14 +15,14 @@ directory_path = 'tests/exports'
 #tablefaker.to_csv("tests/test_table.yaml", "./tests/exports", fake_provider=[SchoolProvider], custom_function=get_level)
 #tablefaker.to_json("tests/test_table.yaml", "./tests/exports", fake_provider=SchoolProvider, custom_function=get_level)
 #tablefaker.to_excel("tests/test_table.yaml", "./tests/exports", fake_provider=SchoolProvider, custom_function=get_level)
-#tablefaker.to_parquet("tests/test_table.yaml", "./tests/exports", fake_provider=SchoolProvider, custom_function=get_level)
+tablefaker.to_parquet("tests/test_table.yaml", "./tests/exports", fake_provider=SchoolProvider, custom_function=get_level)
 #tablefaker.to_sql("tests/test_table.yaml", "./tests/exports", fake_provider=SchoolProvider, custom_function=get_level)
 
-df_dict = tablefaker.to_pandas("tests/test_table.yaml", fake_provider=SchoolProvider, custom_function=get_level)
+#df_dict = tablefaker.to_pandas("tests/test_table.yaml", fake_provider=SchoolProvider, custom_function=get_level)
 
-person_df = df_dict["person"]
+#person_df = df_dict["person"]
 #print(person_df.dtypes)
-print(person_df.head(5))
+#print(person_df.head(5))
 
 # employee_df = df_dict["employee"]
 # print(employee_df.head(5))
