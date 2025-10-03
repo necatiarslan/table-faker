600dd61b7dfd59612f8681423cd851eacbe400f3
Necati Arslan <necatia@gmail.com>
2025-10-03 17:06:32 -0400

    fix bug

--- tablefaker/__init__.py
+++ tablefaker/__init__.py
@@ -1,6 +1,5 @@
 from .tablefaker import to_csv, to_excel, to_json, to_pandas, to_parquet, to_target, to_sql, to_deltalake, yaml_to_json, avro_to_yaml, csv_to_yaml
 from .relationships import generate_relationships
 from .semantic_view import generate_semantic_view
-from .semantic_view_enhanced import generate_semantic_view_enhanced
 from .semantic_model_metrics import generate_model_metrics
 from .llm_client import LLMClient, create_sample_config
---
+++ tests/manual_tests.py
@@ -0,0 +1,42 @@
+import sys, os, shutil
+sys.path.append(os.path.abspath("."))
+
+from tablefaker import tablefaker
+from faker_education import SchoolProvider
+from faker import Faker
+
+fake = Faker()
+def get_level():
+    return f"level {fake.random_int(1, 5)}"
+
+directory_path = 'tests/exports'
+if os.path.isdir(directory_path):
+    shutil.rmtree(directory_path)
+    os.mkdir(directory_path)
+
+os.system('clear')
+
+# tablefaker.to_csv("tests/test_basic_table.yaml", "./tests/exports", fake_provider=[SchoolProvider], custom_function=get_level)
+# tablefaker.to_csv("tests/test_table.json", "./tests/exports", fake_provider=[SchoolProvider], custom_function=get_level)
+# tablefaker.to_csv("tests/test_table.yaml", "./tests/exports", fake_provider=[SchoolProvider], custom_function=get_level)
+# tablefaker.to_csv("tests/test_table.yaml", "./tests/exports", table_name="person", fake_provider=[SchoolProvider], custom_function=get_level)
+# tablefaker.to_csv("tests/test_table.yaml", "./tests/exports/person.csv", table_name="person", fake_provider=[SchoolProvider], custom_function=get_level)
+# tablefaker.to_json("tests/test_table.yaml", "./tests/exports", fake_provider=SchoolProvider, custom_function=get_level)
+# tablefaker.to_sql("tests/test_table.yaml", "./tests/exports", fake_provider=SchoolProvider, custom_function=get_level)
+# tablefaker.to_parquet("tests/test_table.yaml", "./tests/exports", fake_provider=SchoolProvider, custom_function=get_level)
+# tablefaker.to_deltalake("tests/test_table.yaml", "./tests/exports/", fake_provider=[SchoolProvider], custom_function=get_level)
+# tablefaker.to_deltalake("tests/test_table.yaml", "./tests/exports/person/", table_name="person", fake_provider=[SchoolProvider], custom_function=get_level)
+
+# df_dict = tablefaker.to_pandas("tests/test_table.yaml", fake_provider=SchoolProvider, custom_function=get_level)
+# person_df = df_dict["person"]
+# print(person_df.dtypes)
+# print(person_df.head(5))
+
+# employee_df = df_dict["employee"]
+# print(employee_df.head(5))
+
+# tablefaker.avro_to_yaml("tests/test_person.avsc", "tests/exports/person.yaml")
+# tablefaker.to_csv("tests/exports/person.yaml", "./tests/exports/")
+
+# tablefaker.csv_to_yaml("tests/test_person.csv", "tests/exports/person.yaml")
+# tablefaker.to_csv("tests/exports/person.yaml", "./tests/exports/")
